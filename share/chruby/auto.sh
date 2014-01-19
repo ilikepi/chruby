4d478791dfbb6f281d68121560a7620f2491e051
James Blanding <james@basicsgroup.com>
2014-01-19 18:50:20 -0500

    Call chruby_reset when chruby fails to switch

--- share/chruby/auto.sh
+++ share/chruby/auto.sh
@@ -1,15 +1,17 @@
 unset RUBY_AUTO_VERSION
 
 function chruby_auto() {
-	local dir="$PWD" version
+	local dir="$PWD" version chruby_rc
 
 	until [[ -z "$dir" ]]; do
 		if { read -r version <"$dir/.ruby-version"; } 2>/dev/null || [[ -n "$version" ]]; then
-			if [[ "$version" == "$RUBY_AUTO_VERSION" ]]; then return
+			if [[ "$version" == "$RUBY_AUTO_VERSION" && -n "$RUBY_ROOT" ]]; then return
 			else
 				RUBY_AUTO_VERSION="$version"
 				chruby "$version"
-				return $?
+				chruby_rc=$?
+				[[ $chruby_rc == 0 ]] || chruby_reset
+				return $chruby_rc
 			fi
 		fi
 
